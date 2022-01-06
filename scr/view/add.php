8cd325f1e9e73ec73b5b9f80045181235dad9ab8
06_1951060541_NguyenHuuBach <94445131+NguyenHuuBach1951060541@users.noreply.github.com>
2022-01-06 17:44:03 +0700

    Create add.php

---
+++ scr/view/add.php
@@ -0,0 +1,20 @@
+<?php include("template/header.php"); ?>
+    <form method="post">
+        <input type="text" placeholder="Mã môn học" name="txtmamh">
+        <br>
+        <input type="text" placeholder="Tên môn học" name="txttenmonhoc">
+        <br>
+        <input type="text" placeholder="Số tín chỉ" name="txtsotinchi">
+        <br>
+        <input type="text" placeholder="Số tiết lý thuyết" name="txtsotietlythuyet">
+        <br>
+        <input type="text" placeholder="Số tiết bài tập" name="txtsotietbaitap">
+        <br>
+        <input type="text" placeholder="Số tiết thực hành" name="txtsotietthuchanh">
+        <br>
+        <input type="text" placeholder="Số giờ tự học" name="txtsogiotuhoc">
+        <br>
+    </form>
+<?php
+    include("tempalte/footer.php");
+?>
