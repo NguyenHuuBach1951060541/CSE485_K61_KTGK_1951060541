eabaa8684c08d8986d0a820af61688628fd6cd93
06_1951060541_NguyenHuuBach <94445131+NguyenHuuBach1951060541@users.noreply.github.com>
2022-01-06 17:14:47 +0700

    Update controller.php

--- scr/controller/controller.php
+++ scr/controller/controller.php
@@ -1,65 +1,71 @@
 <?php
-    require_once 'config/db.php';
-    class model{
-        private $mamh, $ten_mh, $sotinchi, $sotiet_lt, $sotiet_bt, $sotiet_thtn, $sogio_tuhoc;
+    require_once 'model/model.php';
+    class controller{
+        function index(){
+            $model = new model;
+            $datas = $model->getAllUsers();
+            require_once('view/index.php');
+        }
 
-        public function connectDb() {
-            $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-            if (!$connection) {
-                die("Không thể kết nối. Lỗi: " .mysqli_connect_error());
+        function details(){
+            $model = new model;
+            $id = $_GET['id'];
+            $data = $model->getUser($id);
+            if($data)
+                header("location:index.php");
+                else
+                header("error.php");
+        }
+
+        function add(){
+            require_once 'view/add.php';
+            if(isset($_POST['txtmamh'])){
+                $mamh = $_POST['txtmamh'];
+                $ten_mh = $_POST['txttenmonhoc'];
+                $sotinchi = $_POST['txtsotinchi'];
+                $sotiet_lt = $_POST['txtsotietlythuyet'];
+                $sotiet_bt = $_POST['txtsotietbaitap'];
+                $sotiet_thtn = $_POST['txtsotietthuchanh'];
+                $sogio_tuhoc = $_POST['txtsogiotuhoc'];
+                $model = new model;
+                $result = $model->addUsers($mamh, $ten_mh, $sotinchi, $sotiet_lt, $sotiet_bt, $sotiet_thtn, $sogio_tuhoc);
+                if($result)
+                header("location:index.php");
+                else
+                header("error.php");
             }
-            return $connection;
-        }
-    
-        public function closeDb($connection = null) {
-            mysqli_close($connection);
         }
 
-        public function getAllUsers(){
-            $conn = $this->connectDb();
-            $sql = "SELECT * FROM monhoc";
-            $result = mysqli_query($conn,$sql);
-            $datas = [];
-            if(mysqli_num_rows($result) > 0){
-                $arr_users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        function edit(){
+            $model = new model;
+            $id = $_GET['id'];
+            $data = $model->getUser($id);
+            require_once('view/edit.php');
+            if(isset($_POST['txtmagv'])){
+                $mamh = $_POST['txtmamh'];
+                $ten_mh = $_POST['txttenmonhoc'];
+                $sotinchi = $_POST['txtsotinchi'];
+                $sotiet_lt = $_POST['txtsotietlythuyet'];
+                $sotiet_bt = $_POST['txtsotietbaitap'];
+                $sotiet_thtn = $_POST['txtsotietthuchanh'];
+                $sogio_tuhoc = $_POST['txtsogiotuhoc'];
+                $model = new model;
+                $result = $model->addUsers($mamh, $ten_mh, $sotinchi, $sotiet_lt, $sotiet_bt, $sotiet_thtn, $sogio_tuhoc);
+                if($result)
+                header("location:index.php");
+                else
+                header("error.php");
             }
-            $this->closeDb($conn);
-
-            return $arr_users;
-        }
-        public function addUsers($mamh, $ten_mh, $sotinchi, $sotiet_lt, $sotiet_bt, $sotiet_thtn, $sogio_tuhoc){
-            $conn = $this->connectDb();
-            $sql = "INSERT INTO monhoc(mamh, ten_mh, sotinchi, sotiet_lt, sotiet_bt, sotiet_thtn, sogio_tuhoc) VALUES ('$mamh', '$ten_mh', '$sotinchi', '$sotiet_lt', '$sotiet_bt', '$sotiet_thtn', '$sogio_tuhoc')";
-            $result = mysqli_query($conn,$sql);
-            $this->closeDb($conn);
-            return $result;
-        }
-        public function getUser($id){
-            $conn = $this->connectDb();
-            $sql = "SELECT * FROM monhoc WHERE mamh = '$id'";
-            $result = mysqli_query($conn,$sql);
-            if(mysqli_num_rows($result) > 0)
-                $user = mysqli_fetch_assoc($result);
-            $this->closeDb($conn);
-            return $user;
         }
 
-        public function updateUser($mamh, $ten_mh, $sotinchi, $sotiet_lt, $sotiet_bt, $sotiet_thtn, $sogio_tuhoc){
-            $conn = $this->connectDb();
-            $sql = "UPDATE monhoc SET mamh='$mamh', ten_mh='$ten_mh', sotinchi='$sotinchi', sotiet_lt='$sotiet_lt', sotiet_bt='$sotiet_bt', sotiet_thtn='$sotiet_thtn', sogio_tuhoc='$sogio_tuhoc' WHERE bd_id = ''";
-            $result = mysqli_query($conn,$sql);
-            $this->closeDb($conn);
-            return $result;
-        }
-
-        function deleteUser($id){
-            $conn = $this->connectDb();
-
-            $sql = "DELETE FROM monhoc WHERE magv = '$id'";
-            $result = mysqli_query($conn,$sql);
-
-            $this->closeDb($conn);
-            return $result;
+        function delete(){
+            $model = new model;
+            $id = $_GET['id'];
+            $result = $model->deleteUser($id);
+            if($result)
+                header("location:index.php");
+                else
+                header("error.php");
         }
     }
 ?>
