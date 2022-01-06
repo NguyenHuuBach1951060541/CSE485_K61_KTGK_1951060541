043dead0a28a48d759cd7aa7a7c6f2884c988455
06_1951060541_NguyenHuuBach <94445131+NguyenHuuBach1951060541@users.noreply.github.com>
2022-01-06 17:44:04 +0700

    Create detail.php

---
+++ scr/view/detail.php
@@ -0,0 +1,22 @@
+<?php include("tempalte/header.php");
+    echo $data['mamh'];
+    echo('<br>');
+    echo $data['tenmh'];
+    echo('<br>');
+    echo $data['sotinchi'];
+    echo('<br>');
+    echo $data['sotiet_lt'];
+    echo('<br>');
+    echo $data['sotiet_bt'];
+    echo('<br>');
+    echo $data['sotiet_thtn'];
+    echo('<br>');
+    echo $data['sogio_tuhoc'];
+    echo('<br>');
+?>
+
+<a href="index.php">Back</a>
+
+<?php
+    include("template/footer.php")
+?>
